02c6487de5dc65e2ab1be91e17e1d79a79340787
Alvaro Flores <asflum@proton.me>
2025-02-04 16:47:03 -0500

    Tercera versión con la inclusión de 2 nuevas columnas para el tipo de papa y el tipo de corte

--- app/src/main/java/com/asflum/cocina/AppDatabase.kt
+++ app/src/main/java/com/asflum/cocina/AppDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [SavedConfig::class], version = 1, exportSchema = false)
+@Database(entities = [SavedConfig::class], version = 3, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun savedConfigDao(): SavedConfigDao
@@ -20,7 +20,8 @@ abstract class AppDatabase : RoomDatabase() {
                     context.applicationContext,
                     AppDatabase::class.java,
                     "app_database"
-                ).build().also { INSTANCE = it }
+                )
+                    .build().also { INSTANCE = it }
             }
         }
     }
