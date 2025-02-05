f3c01c8afdd517cbff8487404104bc00bc9334f6
Alvaro Flores <asflum@proton.me>
2025-02-05 16:58:48 -0500

    Adición de dos nuevas fuentes: Poppins y Quicksand

--- app/src/main/java/com/asflum/cocina/ui/theme/Type.kt
+++ app/src/main/java/com/asflum/cocina/ui/theme/Type.kt
@@ -2,9 +2,31 @@ package com.asflum.cocina.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.asflum.cocina.R
+
+val poppinsFamily = FontFamily(
+    Font(R.font.poppins_regular, FontWeight.Normal),
+    Font(R.font.poppins_bold, FontWeight.Bold),
+    Font(R.font.poppins_thin, FontWeight.Thin),
+    Font(R.font.poppins_black, FontWeight.Black),
+    Font(R.font.poppins_semibold, FontWeight.SemiBold),
+    Font(R.font.poppins_extrabold, FontWeight.ExtraBold),
+    Font(R.font.poppins_extralight, FontWeight.ExtraLight),
+    Font(R.font.poppins_medium, FontWeight.Medium),
+    Font(R.font.poppins_light, FontWeight.Light)
+)
+
+val quicksandFamily = FontFamily(
+    Font(R.font.quicksand_bold, FontWeight.Bold),
+    Font(R.font.quicksand_light, FontWeight.Light),
+    Font(R.font.quicksand_regular, FontWeight.Normal),
+    Font(R.font.quicksand_semibold, FontWeight.SemiBold),
+    Font(R.font.quicksand_medium, FontWeight.Medium)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
