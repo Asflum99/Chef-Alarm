54c7c76bcab66bb3f0cfa9898b06a16948ed4eb2
Alvaro Flores <asflum@proton.me>
2025-02-05 11:49:27 -0500

    Cambio de configuración en los títulos de las páginas y adición de efecto de transición de colores

--- app/src/main/java/com/asflum/cocina/MainActivity.kt
+++ app/src/main/java/com/asflum/cocina/MainActivity.kt
@@ -8,24 +8,30 @@ import android.provider.AlarmClock.ACTION_SET_ALARM
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -33,7 +39,9 @@ import com.asflum.cocina.ui.theme.CocinaTheme
 import kotlinx.coroutines.launch
 import com.asflum.cocina.pages.page1.Page1
 import com.asflum.cocina.pages.page2.Page2
+import com.asflum.cocina.ui.theme.DarkGray
 import com.asflum.cocina.ui.theme.MustardYellow
+import com.asflum.cocina.ui.theme.TomatoRed
 import com.asflum.cocina.ui.theme.WarmWhite
 
 class MainActivity : ComponentActivity() {
@@ -74,42 +82,67 @@ fun NavigationComponent() {
     )
 
     val coroutineScope = rememberCoroutineScope()
-
     val currentPage = pagerState.currentPage
 
+    // Animación de transición de colores
+    val text1Color by animateColorAsState(
+        targetValue = if (currentPage == 0) TomatoRed else WarmWhite,
+        animationSpec = tween(durationMillis = 200)
+    )
+    val text2Color by animateColorAsState(
+        targetValue = if (currentPage == 1) TomatoRed else WarmWhite,
+        animationSpec = tween(durationMillis = 200)
+    )
+
+    // Obtener altura de la barra de estado
+    val statusBarHeight = WindowInsets.statusBars.asPaddingValues().calculateTopPadding()
+
     Column(modifier = Modifier.fillMaxSize()) {
+        // Barra de estado (para cubrir la parte superior del celular)
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(statusBarHeight) // Solo cubre la barra de estado
+                .background(DarkGray)
+        )
+
         // Parte superior fija
         Box(
             modifier = Modifier
                 .fillMaxWidth()
-                // Porcentaje de espacio para la parte superior
-                .weight(0.2f)
-                .background(MustardYellow)
+                .weight(0.18f) // Porcentaje de espacio para la parte superior
+                .background(MustardYellow),
+            contentAlignment = Alignment.Center // Los elementos dentro del Box (Row) estará centrado
         ) {
             Row(
                 modifier = Modifier
-                    .align(Alignment.Center)
-                    .padding(horizontal = 16.dp)
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
             ) {
                 Text(text = "Alimentos guardados",
-                    fontSize = if (currentPage == 0) 20.sp else 14.sp,
-                    color = if (currentPage == 0) Color.Black else Color.Gray,
-                    modifier = Modifier.clickable {
+                    fontSize = 20.sp,
+                    color = text1Color,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .width(120.dp)
+                        .clickable {
                         coroutineScope.launch {
                             pagerState.animateScrollToPage(0)
                         }
-                    })
-
-                Spacer(modifier = Modifier.width(16.dp))
-
+                    }
+                )
                 Text(text = "Añadir alimento",
-                    fontSize = if (currentPage == 1) 20.sp else 14.sp,
-                    color = if (currentPage == 1) Color.Black else Color.Gray,
-                    modifier = Modifier.clickable {
+                    fontSize = 20.sp,
+                    color = text2Color,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .width(120.dp)
+                        .clickable {
                         coroutineScope.launch {
                             pagerState.animateScrollToPage(1)
                         }
-                    })
+                    }
+                )
             }
         }
         HorizontalPager(
